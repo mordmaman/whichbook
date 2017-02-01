c3f05d5479098851b2834e40a7179794d642ec94
Mord Maman <jaime.mord.maman@gmail.com>
2017-02-01 15:47:02 +0000

    replaces request with axios

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 var request = require('request');
 var parseString = require('xml2js').parseString;
+var axios = require('axios')
 
-request('https://www.goodreads.com/book/title.xml?key=M0mPzFH0hVT5EgajAgrA&title=Harry+Potter', function (error, response, body) {
-  if (!error && response.statusCode == 200) { 
-    parseString(body, function (err, result) {
-        console.log(result.GoodreadsResponse.book[0].average_rating[0]);
+// axios.get('https://www.goodreads.com/book/title.xml?key=M0mPzFH0hVT5EgajAgrA&title=Harry+Potter' )
+//   .then(function(response){
+//     console.log(response); // ex.: { user: 'Your User'}
+//     console.log(response.status); // ex.: 200
+//   });  
+
+
+axios.get('https://www.goodreads.com/book/title.xml?key=M0mPzFH0hVT5EgajAgrA&title=Harry+Potter')
+  .then(function (response) {
+
+    // console.log(response.data);
+
+    parseString(response.data, function (err, result) {
+      console.log(result.GoodreadsResponse.book[0].average_rating[0]);
     });
-  }
-})
+    // console.log(response); // ex.: { user: 'Your User'}
+    // console.log(response.status); // ex.: 200
+  });
+
+// request('https://www.goodreads.com/book/title.xml?key=M0mPzFH0hVT5EgajAgrA&title=Harry+Potter', function (error, response, body) {
+//   if (!error && response.statusCode == 200) { 
+//     parseString(body, function (err, result) {
+//         console.log(result.GoodreadsResponse.book[0].average_rating[0]);
+//     });
+//   }
+// });
+
+// var tag = document.querySelector("p");
+// tag.textContent = "blah blah blah";
+
