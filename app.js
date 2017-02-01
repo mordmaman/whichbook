a3902c4cd224c1c3ce0d1c585f419b1d4b1fae70
Mord Maman <jaime.mord.maman@gmail.com>
2017-02-01 11:29:34 +0000

    displays the first level of the object

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ request('https://www.goodreads.com/book/title.xml?key=M0mPzFH0hVT5EgajAgrA&title
     //var parsedData = JSON.parse(body);
     //console.log(body);  
     parseString(body, function (err, result) {
-        console.dir(JSON.stringify(result));
+      var string = JSON.stringify(result);
+      var object = JSON.parse(string)
+        console.dir(object.GoodreadsResponse);
     });
   }
 })
