48584ea61306121e0c5a69b3294cf3ed88dcecff
Mord Maman <jaime.mord.maman@gmail.com>
2017-02-02 11:33:01 +0000

    sends request in console

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var parseString = require('xml2js').parseString;
-var axios = require('axios')
+var axios = require('axios');
+var express = require('express');
+var app = express();
 
-axios.get('https://www.goodreads.com/book/title.xml?key=M0mPzFH0hVT5EgajAgrA&title=Harry+Potter')
-  .then(function (response) {
-    parseString(response.data, function (err, result) {
-      console.log(result.GoodreadsResponse.book[0].average_rating[0]);
+
+app.get('/search', function(req, res) {
+    axios.get('https://www.goodreads.com/book/title.xml?key=M0mPzFH0hVT5EgajAgrA&title=Harry+Potter')
+    .then(function (response) {
+      parseString(response.data, function (err, result) {
+        console.log(result.GoodreadsResponse.book[0].average_rating[0]);
+      });
     });
   });
 
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
 
